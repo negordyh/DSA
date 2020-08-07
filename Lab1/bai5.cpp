68469cb47e8fc731c1491c09146514c642798de2
Duyen <vanduyen.mai@gmail.com>
2020-08-07 13:47:32 +0700

    lcs by recursion

--- Lab1/bai5.cpp
+++ Lab1/bai5.cpp
@@ -9,37 +9,30 @@
 using namespace std;
 
 
-int lengthOfLonggestCommonSubstring(const char* x, const char* y, int m, int n) {
+int lcs(const char* x, const char* y, int m, int n) {
 	// TODO
-int lengthOfLonggestCommonSubstring[m+1][n+1]; 
-    int result = 0; 
-
-    for (int i=0; i<=m; i++) 
-    { 
-        for (int j=0; j<=n; j++) 
-        { 
-            if (i == 0 || j == 0) 
-                lengthOfLonggestCommonSubstring[i][j] = 0; 
-  
-            else if (x[i-1] == y[j-1]) 
-            { 
-                lengthOfLonggestCommonSubstring[i][j] = lengthOfLonggestCommonSubstring[i-1][j-1] + 1; 
-                result = max(result, lengthOfLonggestCommonSubstring[i][j]); 
-            } 
-            else lengthOfLonggestCommonSubstring[i][j] = 0; 
-        } 
-    } 
-    return result; 
+    int count = 0;
+    if (m == 0 || n == 0)  
+        return count;  
+    if (x[m-1] == y[n-1])  
+        count = 1 + lcs(x, y, m-1, n-1);  
+    else
+        count = max(lcs(x, y, m, n-1), lcs(x, y, m-1, n));  
+    return count;
 }
 
+int max(int a, int b)  
+{  
+    return (a > b)? a : b;  
+}  
 
 int main() {
         string x;
         string y;
-	    x = "abc123789";
-        y = "abc123456";
+	    x = "mvdxyz";
+        y = "xyzabcd";
 
-	cout << lengthOfLonggestCommonSubstring(x.c_str(), y.c_str(), x.length(), y.length());
+	cout << lcs(x.c_str(), y.c_str(), x.length(), y.length());
 
 	return 0;
 
